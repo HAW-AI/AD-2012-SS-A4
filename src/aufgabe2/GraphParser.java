dda74f464e1a9c9e01aeb7e2c7d0d420c4897e82
Daniela Barths <daniela.barths@googlemail.com>
2012-06-25 20:29:35 +0200

    aufgabe2

---
+++ src/aufgabe2/GraphParser.java
@@ -0,0 +1,68 @@
+package aufgabe2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.jgrapht.Graph;
+import org.jgrapht.WeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.DirectedWeightedMultigraph;
+
+public class GraphParser {
+
+	//singleton-pattern: nur eine Instanz dieser Klasse erstellbar 
+	private static GraphParser instance = new GraphParser();
+	
+	//Sichtbarkeit des Konstruktors einschraenken
+	private GraphParser(){ 
+	}
+	
+	//statische getInstance Methode
+	public static GraphParser getInstance(){
+		return instance;
+	}
+	
+	private boolean isGraphFile(String filepath){
+		return filepath.endsWith("graph");
+	}
+	
+	public Graph<String, DefaultWeightedEdge> loadGraphFrom(String filepath) throws FileNotFoundException{
+		
+		//pruefen, ob es sich um ein .graph file handelt 
+		if (!isGraphFile(filepath)){
+			throw new IllegalArgumentException("'" + filepath + "' is not a .graph file");
+		}
+		
+		File file = new File(filepath);
+		Scanner graphdata;
+		
+		//wirft eine FileNotFoundException, wenn File nicht vorhanden
+		graphdata = new Scanner(file);
+
+		WeightedGraph<String, DefaultWeightedEdge> graph = new DirectedWeightedMultigraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		DefaultWeightedEdge aktEdge;
+		
+
+		//Graph einlesen:
+		while(graphdata.hasNext()){
+			String aktSting = graphdata.next().trim();
+								
+			String[] aktAry = aktSting.split(",");
+			String ecke1 = aktAry[0].trim();
+			String ecke2 = aktAry[1].trim();
+			Double weight = Double.parseDouble(aktAry[2].trim());
+			if(!graph.containsVertex(ecke1)){
+				graph.addVertex(ecke1);
+			}
+			if(!graph.containsVertex(ecke2)){
+				graph.addVertex(ecke2);
+			}
+			aktEdge = graph.addEdge(ecke1, ecke2);
+			graph.setEdgeWeight(aktEdge, weight);
+			aktEdge = graph.addEdge(ecke2, ecke1);
+			graph.setEdgeWeight(aktEdge, weight);
+		}
+		return graph;
+	}
+}
---
+++ src/aufgabe2/GraphUtilitys.java
@@ -0,0 +1,134 @@
+package aufgabe2;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+/**
+ * julian und daniela
+ * 
+ * */
+public class GraphUtilitys {
+	
+	
+	//Rueckgabe: Hash-Map mit Kante -> Flu�
+		public static double fordUndFulkerson(Graph<String, DefaultWeightedEdge> graph, String quelle, String senke){
+			
+			//Kante -> Kapazitaet
+			Map<DefaultWeightedEdge, Double> kapazitaet = new HashMap<DefaultWeightedEdge, Double>();
+			for(DefaultWeightedEdge e : graph.edgeSet()){
+				kapazitaet.put(e, graph.getEdgeWeight(e));
+			}
+				
+			//Initialisierung:
+			//Kante -> Flu�
+			Map<DefaultWeightedEdge, Double> fluss = new HashMap<DefaultWeightedEdge, Double>();
+			for(DefaultWeightedEdge e : graph.edgeSet()){
+				fluss.put(e, 0d);
+			}
+			
+			
+			//Initialisierung 2.Teil
+			//Ecke -> vorgaengerEcke
+			Map<String, String> vorgaenger = new HashMap<String, String>();
+			vorgaenger.put(quelle, quelle);
+			//Ecke -> vorzeichen Vorgaengerecke ( + fuer Vorwaerts, - fuer Rueckwaertskante)
+			Map<String, String> kantenrichtung = new HashMap<String, String>();
+			//Ecke -> Inkrement(Double)
+			Map<String, Double> inkrement = new HashMap<String, Double>();
+			inkrement.put(quelle, Double.POSITIVE_INFINITY);
+			//enth�lt alle schon inspizierten Ecken
+			Set<String> inspiziert = new HashSet<String>();
+			
+			
+			//solange noch nicht alle markierten Ecken auch inspiziert wurden 
+			while(inspiziert.size() < vorgaenger.size()){ 
+				//eine beliebige markierte aber noch nicht inzsizierte Ecke waehlen:
+				String eckeZuInspizieren = null;
+				for(String ecke : vorgaenger.keySet()){
+					if(!inspiziert.contains(ecke) && eckeZuInspizieren == null){
+						eckeZuInspizieren = ecke;
+						inspiziert.add(ecke);
+					}
+				}
+				
+				for (String ecke :graph.vertexSet()){
+					if(!vorgaenger.keySet().contains(ecke)){
+						//Vorwaertskante vorhanden und fluss noch erhoehbar
+						if(graph.containsEdge(eckeZuInspizieren, ecke) && kapazitaet.get(graph.getEdge(eckeZuInspizieren, ecke))> fluss.get(graph.getEdge(eckeZuInspizieren, ecke))){
+							vorgaenger.put(ecke, eckeZuInspizieren);
+							kantenrichtung.put(ecke, "+");
+							double minimum = Math.min(inkrement.get(eckeZuInspizieren), (kapazitaet.get(graph.getEdge(eckeZuInspizieren, ecke)) - fluss.get(graph.getEdge(eckeZuInspizieren, ecke))));
+							inkrement.put(ecke, minimum);
+						}
+						//Rueckwaertskante vorhanden und Fluss vorhanden (d.h. erniedrigbar)
+						if(graph.containsEdge(ecke, eckeZuInspizieren) && fluss.get(graph.getEdge(ecke, eckeZuInspizieren)) > 0){
+							vorgaenger.put(ecke, eckeZuInspizieren);
+							kantenrichtung.put(ecke, "-");
+							double minimum = Math.min(inkrement.get(eckeZuInspizieren), fluss.get(graph.getEdge(ecke,eckeZuInspizieren)));
+							inkrement.put(ecke, minimum);
+						}
+					}
+				}
+				
+				//wenn Senke erreicht, den gefundenen vergroe�ernden Weg um das Inkrement erhoehen/erniedrigen, das bei der Senke eingetragen ist  
+				if(vorgaenger.keySet().contains(senke)){
+					double inkr = inkrement.get(senke);
+					String aktEcke = senke;
+					String aktVor;
+					Double aktFluss;
+					
+					//Zum nachvollziehen des Vergroessernden Weges:
+					String weg = aktEcke;
+					
+					//Weg zurueckverfolgen
+					while(aktEcke != quelle){
+						aktVor = vorgaenger.get(aktEcke);
+						if(kantenrichtung.get(aktEcke).equals("+")){ 	//vorwaertskante
+							aktFluss = fluss.get(graph.getEdge(aktVor, aktEcke));
+							fluss.put(graph.getEdge(aktVor, aktEcke), aktFluss + inkr);
+						}else{											//rueckwaertskante
+							aktFluss = fluss.get(graph.getEdge(aktEcke, aktVor));
+							fluss.put(graph.getEdge(aktEcke, aktVor), aktFluss - inkr);
+						}
+						
+						aktEcke = aktVor;
+						
+						weg = aktEcke + ", " + weg;
+					}
+//					
+//					//Zum Nachvollziehen:
+//					System.out.println("Vergroessernder Weg: " + weg + "; Inkrement: " + inkr);
+//					System.out.println(fluss);
+					
+					//und dann auch wieder die Hashes und sets reseten:
+					//Initialisierung 2.Teil wiederholen:
+					//Ecke -> vorgaengerEcke
+					vorgaenger = new HashMap<String, String>();
+					vorgaenger.put(quelle, quelle); //einfach Vorgaenger der Quelle, wie bei Djikstra sich selbst gesetzt
+					//Ecke -> vorzeichen Vorgaengerecke ( + fuer Vorwaerts, - fuer Rueckwaertskante)
+					kantenrichtung = new HashMap<String, String>();
+					//Ecke -> Inkrement(Double)
+					inkrement = new HashMap<String, Double>();
+					inkrement.put(quelle, Double.POSITIVE_INFINITY);
+					//enth�lt alle schon inspizierten Ecken
+					inspiziert = new HashSet<String>();
+				}
+			}
+			
+			double gesFluss = 0;
+			for (Map.Entry<DefaultWeightedEdge, Double> tupel : fluss.entrySet()){
+				if(graph.getEdgeSource(tupel.getKey()).equals(quelle))
+					gesFluss += tupel.getValue();
+					
+			}
+			
+			//ganz am Ende, kein vergroessernder Weg mehr gefunden wurde:	
+			return gesFluss;
+		}
+
+}
